f4d1d3e1d8b400e3e58d03fdbf6750061c31d2f8
Nutk09 <jjoherrera@poligran.edu.co>
2025-04-05 10:20:32 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// Espera a que todo el contenido del DOM esté completamente cargado antes de ejecutar el código
+document.addEventListener('DOMContentLoaded', () => {
+  
+  // Muestra un mensaje en la consola del navegador indicando que la aplicación ha sido cargada
+  console.log("Movesafe app loaded.");
+});
+
